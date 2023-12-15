7fe17425e2f14975cd728f453c19a00ec8b81805
Hjl <1031939925@qq.com>
2023-12-15 09:36:58 +0800

    1

--- app/src/app.js
+++ app/src/app.js
@@ -2,21 +2,10 @@
 // import Control from "./pages/control"
 // import SubAndPub from "./pages/comunication"
 // import Father from "./pages/lifeCycle/index."
-// import Use from "./pages/hooks/useState"
-<<<<<<< HEAD
-// import Parent from "./pages/hooks/useCallback"
-// import Ref1 from "./pages/hooks/useRef"
-// import TodoList from "./pages/hooks/useCallback"
-import Add from "./pages/hooks/useReducer"
+import Use from "./pages/hooks/useState"
 export default function App () {
     return (
-      <Add />
-=======
-import Parent from "./pages/hooks/useCallback"
-export default function App () {
-    return (
-      <Parent />
->>>>>>> 278f78b1ac52b9e746791c34a49f6c0b6a486322
+      <Use />
     )
 }
 
